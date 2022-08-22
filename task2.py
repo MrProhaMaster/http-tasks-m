f53b4badcba6580c3d59f3b7d7ddbfd9e7c1ab99
MrProha <mrprohagames@gmail.com>
2022-08-22 18:58:37 +0300

    task2 - redacted

--- task2.py
+++ task2.py
@@ -1,19 +1,23 @@
 import os
-from yadisk import YaDisk as Y
+import requests
 
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
-    def upload(self, file_path: str):
-        files = []
-        for file in  os.listdir(file_path):
-            files.append(file)
-        disk = Y(token=self.token)
-        for i in files:
-            disk.upload(file_path+'/'+i, i)
-        print('Finished')
-        
+    def get_link(self, filename):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = {'Content-Type': 'application/json', 'Authorization': 'OAuth {}'.format(self.token)}
+        params = {"path": filename, "overwrite": "false"}
+        link = requests.get(upload_url, headers=headers, params=params)
+        return link.json()['href']
+
+    def upload(self, file_path):
+        for file in os.listdir(file_path):
+            url = self._get_upload_link(file)
+            response = requests.put(url, data=open(f'{file_path}/{file}', 'rb'))
+        print('Done')
+
 if __name__ == '__main__':
     path_to_file = 'file'
     token = ''
